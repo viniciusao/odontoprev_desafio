8b5beb3a5e6582d14f7d31e525d45368c126e17e
Vinícius Aguiar <vinicius.aguiar@empiricus.com.br>
2022-02-13 18:08:02 -0300

    refactor: import dos schemas é inicializado pelo package `schemas` no __init__.py

---
+++ src/guias_tratamentos_odontologicos/chalicelib/schemas/__init__.py
@@ -0,0 +1,5 @@
+""" Todas as propriedades nos schemas foram marcadas
+como `obrigatórias` arbitrariamente. """
+
+from .beneficiario import CadastroBeneficiario
+from .dentista import CadastroDentista
