48b69fe2acf55c7c2f38a4778baec811d292af51
Vinícius Aguiar <vinicius.aguiar@empiricus.com.br>
2022-02-13 19:04:22 -0300

    new: script para setup da aplicação

---
+++ setup_hml.sh
@@ -0,0 +1,10 @@
+python -m venv "$PWD"/venv
+source venv/bin/activate
+pip install -e .
+APP_PATH='src/guias_tratamentos_odontologicos'
+python $APP_PATH/chalicelib/cli/setup_database.py
+mypy $APP_PATH/
+pytest -sv
+sleep 5
+cd $APP_PATH || exit
+chalice local --stage hml
