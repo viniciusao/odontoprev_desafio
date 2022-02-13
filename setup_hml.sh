325395d63211a62459381a5db1e82f90148fdcea
Vinícius Aguiar <vinicius.aguiar@empiricus.com.br>
2022-02-13 19:14:16 -0300

    refactor: adicionado um sleep para visualizar mypy output

--- setup_hml.sh
+++ setup_hml.sh
@@ -4,6 +4,7 @@ pip install -e .
 APP_PATH='src/guias_tratamentos_odontologicos'
 python $APP_PATH/chalicelib/cli/setup_database.py
 mypy $APP_PATH/
+sleep 3
 pytest -sv
 sleep 5
 cd $APP_PATH || exit
